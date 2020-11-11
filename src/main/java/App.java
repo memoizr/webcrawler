991e6a24b3a6c951ed77409c523fd528d1cf47be
= <=>
2020-11-11 20:16:00 +0000

    Use Standard Input instead of argument

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,10 +1,15 @@
 import models.Library;
 
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class App {
     public static void main(String[] args) {
-        // Supply the right number of args or else!! Seriously I'm not gonna write a help page
-        // There are nice libraries for that like kotlinx-cli
-        var arg = args[0];
+        System.out.println("Enter query, then press enter");
+        var scanner = new Scanner(System.in);
+        scanner.useDelimiter(Pattern.compile("[\\r\\n;]+"));
+        var arg = scanner.next();
+        scanner.close();
 
         new MostFrequentLibraries(new BingSearchEngine(), new JsoupLibFinder())
                 .fromTerm(arg)
